ab3fc9990f154b99a5ea82dd65a52266290a7ea5
gelofang <afang1@up.edu.ph>
2024-11-06 04:24:20 +0800

    Update index.py

--- index.py
+++ index.py
@@ -5,23 +5,44 @@ import dash_bootstrap_components as dbc
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
-from apps import commonmodules as cm
-from apps import home
-from apps.movies import movie_management, movie_management_profile
 
 # Importing your app variable from app.py so we can use it
 from app import app
+from apps import headnfootTemplate as hft
+from apps import home
+#from apps.headnfootTemplate import create_footer
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+#print(create_header())
+a = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur'
 
 app.layout = html.Div(
     [
-        # Location Variable -- contains details about the url
+        hft.create_header(),
         dcc.Location(id='url', refresh=True),
-
-        cm.navbar,
-
-        # Page Content -- Div that contains page layout
-        html.Div(id='page_content', className='m-2 p-2'),
-    ]
+        html.Div(
+            children=[
+                html.H1("Welcome to Pawssion Project"),
+                html.P(a),
+                html.P(a),
+                html.P(a),
+                html.P(a),
+            ],
+            style={
+                'position':'relative',             # Allow content to take up remaining space
+                'marginTop': '120px',  # Give space for the fixed header (adjust based on header height)
+                'padding': '70px'
+            }
+        ),
+        hft.create_footer()
+    ],
+    style={
+        'display': 'flex',              # Use Flexbox to structure the layout
+        'flexDirection': 'column',      # Stack the header and footer vertically
+        'height': '100vh',              # Ensure the layout takes up the full viewport height
+        'justifyContent': 'space-between',  # Push the header and footer to top and bottom
+    }
 )
 
 @app.callback(
@@ -47,11 +68,29 @@ def displaypage (pathname):
         if pathname == '/' or pathname == '/home':
             returnlayout = home.layout
 
-        elif pathname == '/movies/movie_management':
-            returnlayout = movie_management.layout
+        elif pathname == '/adopt':
+            returnlayout = 'adopt'
         
-        elif pathname == '/movies/movie_management_profile':
-            returnlayout = movie_management_profile.layout
+        elif pathname == '/donate':
+            returnlayout = 'donate'
+
+        elif pathname == '/meettherescues':
+            returnlayout = 'meetTheRescues'
+
+        elif pathname == '/ourstory':
+            returnlayout = 'ourStory'
+
+        elif pathname == '/faqs':
+            returnlayout = 'faqs'
+
+        elif pathname == '/contactus':
+            returnlayout = 'contactUs'
+        
+        elif pathname == '/signin':
+            returnlayout = 'signin'
+
+        elif pathname == '/register':
+            returnlayout = 'register'
 
         else:
             returnlayout = 'error404'
@@ -60,7 +99,6 @@ def displaypage (pathname):
         raise PreventUpdate
     
     return [returnlayout]
-
 
 if __name__ == '__main__':
     webbrowser.open('http://127.0.0.1:8050/', new=0, autoraise=True)
