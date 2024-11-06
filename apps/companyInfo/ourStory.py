9017e74890f8849f238ffe57269e568c6e8b44d7
gelofang <afang1@up.edu.ph>
2024-11-06 20:45:30 +0800

    Update ourStory.py

--- apps/companyInfo/ourStory.py
+++ apps/companyInfo/ourStory.py
@@ -1,1 +1,28 @@
+import dash
+import dash_bootstrap_components as dbc
+from dash import dcc, html
+from dash.exceptions import PreventUpdate
 
+from app import app
+
+# instead of app.layout, we just use the variable "layout" here
+# We cannot really modify the "app" variable here, we only do it in index.py
+layout = html.Div(
+    [
+        html.H2('Welcome to our app!'),
+        html.Hr(),
+        html.Div(
+            [
+                html.Span(
+                    "This is the Our Story Page",
+                ),
+                html.Br(),
+                html.Br(),
+                html.Span(
+                    "Contact the owner if you need assistance!",
+                    style={'font-style':'italic'}
+                ),
+            ]
+        )
+    ], style={"marginTop":"200"}
+)
