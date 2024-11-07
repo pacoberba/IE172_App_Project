f666a90924d580087ed36259c53460063ddc5c6b
gelofang <afang1@up.edu.ph>
2024-11-07 12:01:12 +0800

    Update meetTheRescues.py
    updated with content

--- apps/MeetTheRescues/meetTheRescues.py
+++ apps/MeetTheRescues/meetTheRescues.py
@@ -9,20 +9,69 @@ from app import app
 # We cannot really modify the "app" variable here, we only do it in index.py
 layout = html.Div(
     [
-        html.H2('Welcome to our app!'),
-        html.Hr(),
         html.Div(
             [
-                html.Span(
-                    "This is the Meet the Rescues Modules",
-                ),
-                html.Br(),
-                html.Br(),
-                html.Span(
-                    "Contact the owner if you need assistance!",
-                    style={'font-style':'italic'}
-                ),
+                html.H1("Find your New Bestfur-iend!"),
+                html.Div(
+                    [
+                        dbc.Card(
+                            [
+                                dbc.CardImg(
+                                    src="assets/White.png",
+                                    top=True,
+                                    style={"opacity": 0.3},
+                                    ),
+                                dbc.CardImgOverlay(
+                                    dbc.CardBody(
+                                        [
+                                            html.H4("Card title", className="card-title"),
+                                            html.P(
+                                                "An example of using an image in the background of "
+                                                "a card.",
+                                                className="card-text",
+                                            ),
+                                            dbc.Button("Go somewhere", color="primary"),
+                                        ],
+                                    ),
+                                ),
+                            ],
+                            style={"width": "18rem"},
+                        ),
+
+                        dbc.Card(
+                            [
+                                dbc.CardImg(
+                                    src="assets/White.png",
+                                    top=True,
+                                    style={"opacity": 0.3},
+                                    ),
+                                dbc.CardImgOverlay(
+                                    dbc.CardBody(
+                                        [
+                                            html.H4("Card title", className="card-title"),
+                                            html.P(
+                                                "An example of using an image in the background of "
+                                                "a card.",
+                                                className="card-text",
+                                            ),
+                                            dbc.Button("Go somewhere", color="primary"),
+                                        ],
+                                    ),
+                                ),
+                            ],
+                            style={"width": "18rem"},
+                        )
+                    ]
+                )
+            ]
+        ),
+        html.Div(
+            [
+                html.P("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                html.P("[INTRO ABOUT THE PETS WE HAVE AND HOW MANY ARE RESCUED ETC]"),
+                html.P("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                html.P("[DETAILS ABOUT THE SHELTERS]")
             ]
         )
-    ], style={"marginTop":"200"}
+    ]
 )
