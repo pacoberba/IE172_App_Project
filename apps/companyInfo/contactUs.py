3c20d0c0a3aefafc9a1465bdffef1aa57b71c001
gelofang <afang1@up.edu.ph>
2024-11-07 11:58:23 +0800

    Update contactUs.py
    added content

--- apps/companyInfo/contactUs.py
+++ apps/companyInfo/contactUs.py
@@ -5,24 +5,29 @@ from dash.exceptions import PreventUpdate
 
 from app import app
 
+link = {'color': 'blue'}
 # instead of app.layout, we just use the variable "layout" here
 # We cannot really modify the "app" variable here, we only do it in index.py
 layout = html.Div(
     [
-        html.H2('Welcome to our app!'),
-        html.Hr(),
-        html.Div(
+        html.H1("Contact Us"),
+        html.P(
             [
-                html.Span(
-                    "This is the COntact Us Page.",
-                ),
+                "For questions and general inquiries you can email us at:",
+                html.A("info@pawssionproject.org", href="mailto:info@pawssionproject.org", style=link),
                 html.Br(),
+                "For Adoption inquiries:",
+                html.A("adoption@pawssionproject.org", href="mailto:adoption@pawssionproject.org", style=link),
                 html.Br(),
-                html.Span(
-                    "Contact the owner if you need assistance!",
-                    style={'font-style':'italic'}
-                ),
+                "For questions about our Sponsor-A-Rescue Program:",
+                html.A("sponsorships@pawssionproject.org", href="mailto:sponsorships@pawssionproject.org", style=link),
+                html.Br(),
+                "If you’re interested in building a partnership with Pawssion Project:",
+                html.A("partners@pawssionproject.org", href="mailto:partners@pawssionproject.org", style=link),
+                html.Br(),
+                "For urgent concerns, please send a message to our Facebook page for guidance on the appropriate next steps:"
             ]
         )
-    ], style={"marginTop":"200"}
+    ]
+    
 )
