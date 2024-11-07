d8fd155591ca1600c75d69818d5bf6d0a0644d50
gelofang <afang1@up.edu.ph>
2024-11-07 13:36:51 +0800

    Update register.py

--- apps/register/register.py
+++ apps/register/register.py
@@ -7,22 +7,4 @@ from app import app
 
 # instead of app.layout, we just use the variable "layout" here
 # We cannot really modify the "app" variable here, we only do it in index.py
-layout = html.Div(
-    [
-        html.H2('Welcome to our app!'),
-        html.Hr(),
-        html.Div(
-            [
-                html.Span(
-                    "This is the Registration Page",
-                ),
-                html.Br(),
-                html.Br(),
-                html.Span(
-                    "Contact the owner if you need assistance!",
-                    style={'font-style':'italic'}
-                ),
-            ]
-        )
-    ], style={"marginTop":"200"}
-)
+layout = html.Div(style={"marginTop":"133px"})
